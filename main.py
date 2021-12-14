ed4372a98bf48d194df5e8c628fc5f14c8d989c6
Anshveer Singh <notanshveer@gmail.com>
2021-12-14 22:36:39 +0530

    Add wishMe() function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[0].id)
@@ -7,5 +8,21 @@ engine.setProperty('voices', voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    # checking what part of the day it is
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+
+    speak("I am Jarvis sir, how can I help you today?")
+
+
 if __name__ == '__main__':
     speak("This works, congrats!")
+    wishMe()
